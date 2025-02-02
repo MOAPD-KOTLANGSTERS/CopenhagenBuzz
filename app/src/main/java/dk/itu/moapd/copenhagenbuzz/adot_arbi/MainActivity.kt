46aacac03235b239edd85c519c801f4d603df003
André Birk <arbi@itu.dk>
2025-02-02 16:02:38 +0100

    added date, type and description

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/adot_arbi/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/adot_arbi/MainActivity.kt
@@ -1,8 +1,11 @@
 package dk.itu.moapd.copenhagenbuzz.adot_arbi
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import dk.itu.moapd.copenhagenbuzz.adot_arbi.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val editTextDate = findViewById<TextInputEditText>(R.id.edit_text_date)
+
+        editTextDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                editTextDate.setText(selectedDate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
     }
 }
