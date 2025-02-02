19cff187a44a57d769360b8bcac243ffad609c89
André Birk <arbi@itu.dk>
2025-02-02 15:09:06 +0100

    changed some things in xml

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/adotarbi/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/adotarbi/MainActivity.kt
@@ -11,9 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dk.itu.moapd.copenhagenbuzz.adotarbi.ui.theme.Exercise1Theme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
